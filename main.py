4e20f2b35da6a16d1b5b8de00f4427a90173ba10
thilanhasithaAI <thilanhasithayasanjith26@gmail.com>
2026-08-08 14:14:55 +0530

    web scrapping api tool was created

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from src.tools.tools import web_search
+from src.tools.tools import web_search,scrape_url
 
 output = web_search.run("Latest advancements in AI technology")
 print(output)
+
+result = scrape_url.run(" https://online-engineering.case.edu/blog/advancements-in-artificial-intelligence-and-machine-learning")
+print(result)
