6ed2396b815773763b6c9362df5b773367df4922
thilanhasithaAI <thilanhasithayasanjith26@gmail.com>
2026-08-09 15:55:28 +0530

    production ready

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from src.tools.tools import web_search,scrape_url
+# from src.tools.tools import web_search,scrape_url
 
-output = web_search.run("Latest advancements in AI technology")
-print(output)
+# output = web_search.run("Latest advancements in AI technology")
+# print(output)
 
-result = scrape_url.run(" https://online-engineering.case.edu/blog/advancements-in-artificial-intelligence-and-machine-learning")
-print(result)
+# result = scrape_url.run(" https://online-engineering.case.edu/blog/advancements-in-artificial-intelligence-and-machine-learning")
+# print(result)
+
+from src.pipelines.pipelines import run_research_pipeline
+
+topic = "The impact of artificial intelligence on healthcare"
+run_research_pipeline(topic)
